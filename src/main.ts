37314cc54615fa67cd736609844446c365f2dc67
David-Deve <sopheaktra26@gmail.com>
2025-07-22 19:46:46 +0700

    fix: fix bug

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,5 @@ import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 
 const app = createApp(App);
-app.use(ElementPlus); // <-- use ElementPlus BEFORE mounting
+app.use(ElementPlus);
 app.mount("#app");
